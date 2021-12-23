d823e1ad3fbe22b1d798632f23a230f8e106206b
alejauksiunas <74539838+alejauksiunas@users.noreply.github.com>
2021-12-23 11:59:33 +0200

    Add files via upload

---
+++ kodas-war/kontrolinis1/src/test/java/com/akademijakontrolinis/kontrolinis1/Kontrolinis1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akademijakontrolinis.kontrolinis1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kontrolinis1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
